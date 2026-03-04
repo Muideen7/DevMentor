f3ca9deeec255b741c849bee7f84ae1806baf85e
Frontendgeek <olayeyeayomide2000@gmail.com>
2026-03-04 06:45:53 +0100

    fixed deployment errors

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -41,7 +41,7 @@ export async function POST(req: Request) {
   } catch (error: any) {
     if (error instanceof z.ZodError) {
       return NextResponse.json(
-        { success: false, error: error.errors[0].message, code: 400 },
+        { success: false, error: error.issues[0].message, code: 400 },
         { status: 400 }
       )
     }
